3ddd740d79f891cd7f447a3b459e82ca78d6688e
Karl Damus <getaholdofkarl@gmail.com>
2021-05-26 22:48:00 -0400

    Create Solution.java

---
+++ Hard/TwentyFourGame/Solution.java
@@ -0,0 +1,30 @@
+package TwentyFourGame;
+
+/**
+ * Solution to the 24 Game Problem
+ * https://leetcode.com/problems/24-game/
+ *
+ * @author Karl Damus
+ *
+ * Given 4 cards, with numbers 1-9, determine whether they could reach a value of 24
+ *  let {a, b, c, d}
+ *  let x be any operation
+ *
+ *
+ * runtime: ms (xx test cases)
+ */
+public class Solution {
+    public static void main(String[] args) {
+        int[] nums = new int[] { 4,1, 8, 7 };
+        int[] nums2 = new int[] { 1, 2, 1 ,2 };
+
+        System.out.println(judgePoint24(nums));
+    }
+    public static boolean judgePoint24(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i ++) {
+            if ()
+        }
+
+        return true;
+    }
+}
